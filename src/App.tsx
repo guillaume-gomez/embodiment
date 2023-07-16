a856d989da493b801b5f94a91297e8ce6407cc4c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-07-16 02:28:19 +0200

    use mondiran3d hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 import './App.css';
-import useMondrian from "./hooks/useMondrian";
+import use3DMondrian from "./hooks/use3DMondrian";
 import { customRectString } from "./utils";
 
 function App() {
-   const { generate, rects } = useMondrian();
+   const { generate, mondrians } = use3DMondrian();
 
   return (
     <div className="flex flex-col gap-2 items-center">
-    <h1 className="text-3xl font-bold underline">
-      Embodiment
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        Embodiment
+      </h1>
       <button className="btn btn-accent" onClick={() => generate(1000,1000, 1)}>Generate</button>
-      <div className="card bg-primary text-primary-content">
-         <div className="card-body">
-          <h2 className="card-title">Result</h2>
-          <div className="mockup-code">
-            {
-              rects.map(rect => 
-                (<pre>
-                  <code>{customRectString(rect)}</code>
-                </pre>)
-              )
-            }
-          </div>
-        </div>
-      </div>
+      {
+        mondrians.map(mondrian =>
+          (
+            <div className="card bg-primary text-primary-content">
+               <div className="card-body">
+                <h2 className="card-title">Result</h2>
+                <div className="mockup-code">
+                  {
+                    mondrian.rects.map(rect =>
+                      (<pre>
+                        <code>{customRectString(rect)}</code>
+                      </pre>)
+                    )
+                  }
+                </div>
+              </div>
+            </div>
+          )
+        )
+      }
     </div>
   )
 }
