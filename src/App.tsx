1b827c2e9b06e0240cfc5844fff5e918824f2c35
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-08-12 16:39:20 +0200

    change how we display the mondiran

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,16 @@ function App() {
             <div className="card bg-primary text-primary-content">
                <div className="card-body">
                 <h2 className="card-title">{mondrian.title}</h2>
+                <div>
+                   <MondrianCanvas
+                      ref={canvasActionsRef}
+                      width={width}
+                      height={height}
+                      thickness={2}
+                      rects={mondrian.rects}
+                      toggleFullScreen={toggleFullscreen}
+                    />
+                </div>
                 <div className="mockup-code">
                   {
                     mondrian.rects.map(rect =>
@@ -68,16 +78,6 @@ function App() {
                     }
                   </>
                 }
-                <div>
-                   <MondrianCanvas
-                      ref={canvasActionsRef}
-                      width={width}
-                      height={height}
-                      thickness={2}
-                      rects={mondrian.rects}
-                      toggleFullScreen={toggleFullscreen}
-                    />
-                </div>
               </div>
             </div>
           )
