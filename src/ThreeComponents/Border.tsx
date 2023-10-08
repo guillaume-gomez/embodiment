caba752f9fa59c3cb5a6bb5adf53e1d9269e2889
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-10-08 23:49:56 +0200

    simplify the border concept

---
+++ src/ThreeComponents/Border.tsx
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+import { useMemo } from 'react';
+import {  useThree } from '@react-three/fiber';
+import { CustomRect, heightRect, widthRect, centerRect } from "../utils";
+
+interface BorderProps {
+  position: [number, number, number];
+  geometry:THREE.BoxGeometry;
+  material: THREE.MeshStandardMaterial;
+  meshProps?: JSX.IntrinsicElements['mesh'];
+}
+
+function BorderRect({ position, geometry, material, meshProps }: BorderProps) {
+  return (
+    <mesh
+      material={material}
+      geometry={geometry}
+      position={position}
+      {...meshProps}
+    />
+  )
+}
+
+export default Border;
---
+++ src/ThreeComponents/Borders.tsx
@@ -0,0 +1,21 @@
+import { CustomRect } from "../utils";
+import BorderRect from "./Border";
+
+interface BordersProps {
+  rects: CustomRect[];
+  thickness: number;
+  depth: number;
+}
+
+function Borders({rects, thickness, depth }: BordersProps) {
+  return (
+   <group
+     position={[-0.5, 0.5, 0]}
+   >
+      {rects.map((rect, index) => <BorderRect key={index} rect={rect} thickness={thickness} depth={depth} />) }
+      
+   </group>
+  )
+}
+
+export default Borders;
