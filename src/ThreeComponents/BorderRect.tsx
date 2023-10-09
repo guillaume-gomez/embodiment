d3630b9e985aa8ed76a733ecbf2eea288e262232
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-10-09 19:40:15 +0200

    fix indent

--- src/ThreeComponents/BorderRect.tsx
+++ src/ThreeComponents/BorderRect.tsx
@@ -17,66 +17,71 @@ function BorderRect({rect, thickness, depth, meshProps}: BorderRectProps) {
   const { size: { width, height } } = useThree();
   const thicknessWidth = useMemo(() => thickness/width , [thickness, width]);
   const thicknessHeight = useMemo(() => thickness/height , [thickness, height]);
-  const widthGeometry = useMemo(() => new THREE.BoxGeometry( widthRect(rect)/ width, thicknessHeight, depth ) , [rect, width, thicknessHeight, depth]);
+  const widthGeometry : THREE.BoxGeometry = useMemo(() => new THREE.BoxGeometry( widthRect(rect)/ width, thicknessHeight, depth ) , [rect, width, thicknessHeight, depth]);
   const heightGeometry = useMemo(() => new THREE.BoxGeometry( thicknessWidth, heightRect(rect) / height, depth ) , [rect, height, thicknessWidth, depth]);
   const [x, y] = useMemo(() => centerRect(rect), [rect]);
 
   return (
     <>
     {
-      (rect.y1 > 0) && <Border
-      material={material}
-      geometry={widthGeometry}
-      position={[
-        (rect.x1 + x)/ width,
-        -(rect.y1 + thicknessHeight)/height,
-        depth/2
-      ]
-     }
-      {...meshProps}
-    />
+      (rect.y1 > 0) && 
+      <Border
+        material={material}
+        geometry={widthGeometry}
+        position={[
+          (rect.x1 + x)/ width,
+          -(rect.y1 + thicknessHeight)/height,
+          depth/2
+        ]
+       }
+        {...meshProps}
+     />
+    }
+
+    {
+      (rect.y2 < height) &&
+     <Border
+        material={material}
+        geometry={widthGeometry}
+        position={[
+          (rect.x1 + x)/ width ,
+          -(rect.y2 + thicknessHeight)/height,
+          depth/2
+        ]
+       }
+        {...meshProps}
+      />
     }
 
 
-    {(rect.y2 < height) && <Border
-      material={material}
-      geometry={widthGeometry}
-      position={[
-        (rect.x1 + x)/ width ,
-        -(rect.y2 + thicknessHeight)/height,
-        depth/2
-      ]
-     }
-      {...meshProps}
-    />
+    {(rect.x1 > 0) &&
+      <Border
+        material={material}
+        geometry={heightGeometry}
+        position={[
+          (rect.x1 + thicknessWidth)/ width,
+           -(rect.y1 + y)/height,
+           depth/2
+
+        ]
+       }
+        {...meshProps}
+      />
     }
 
-
-    {(rect.x1 > 0) && <Border
-      material={material}
-      geometry={heightGeometry}
-      position={[
-        (rect.x1 + thicknessWidth)/ width,
-         -(rect.y1 + y)/height,
-         depth/2
-
-      ]
-     }
-      {...meshProps}
-    />
-    }
-
-    {(rect.x2 < width) && <Border
-      material={material}
-      geometry={heightGeometry}
-      position={[
-        (rect.x2 + thicknessWidth)/ width ,
-        -(rect.y1 + y)/height,
-        depth/2
-      ]
-     }
-      {...meshProps}
-    />
+    {
+      (rect.x2 < width) &&
+      <Border
+        material={material}
+        geometry={heightGeometry}
+        position={[
+          (rect.x2 + thicknessWidth)/ width ,
+          -(rect.y1 + y)/height,
+          depth/2
+        ]
+       }
+        {...meshProps}
+      />
     }
     </>
 
