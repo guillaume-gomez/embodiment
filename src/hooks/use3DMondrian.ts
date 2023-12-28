ab7882ec282b61edf4878b7760b55fd39629aa68
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-12-28 22:16:48 +0100

    add cut in subElement

--- src/hooks/use3DMondrian.ts
+++ src/hooks/use3DMondrian.ts
@@ -44,6 +44,15 @@ function use3DMondrian() {
     }
   }
 
+  function subCutIn(customRects3D: CustomRect3D[], axis: AxisType, coord: number) {
+    const choose = sample(customRects3D);
+    const others = rectsWithoutCandidate(customRects3D, choose);
+
+    const partition = cutIn([choose], axis, coord);
+
+    return [...partition, ...others];
+  }
+
   function cutInXY(customRects3D: CustomRect3D[], coord: number) {
     const [selectedCustomsRects3D, others] = partition(customRects3D, (customRect3D) => coord >= customRect3D.z1 && coord <= customRect3D.z2);
 
@@ -108,23 +117,35 @@ function use3DMondrian() {
     return [...flatten(result), ...others];
   }
 
+  function rectsWithoutCandidate(customRects: CustomRect3D[], candidate: CustomRect3D) : CustomRect[] {
+
+    return customRects.filter(customRect =>
+      customRect.x1 !== candidate.x1 ||
+      customRect.x2 !== candidate.x2 ||
+      customRect.y1 !== candidate.y1 ||
+      customRect.y2 !== candidate.y2 ||
+      customRect.z1 !== candidate.z1 ||
+      customRect.z2 !== candidate.z2
+    );
+  }
+
   function generate() {
-    const customRect3D : CustomRect3D = {
+    const init : CustomRect3D = {
       x1: 0, x2: width,
       y1: 0, y2: height,
       z1: 0, z2: depth,
       color: "red"
     };
 
-    //for(let i =0; i < 3; i++) {
-      const randomCoord = Math.floor(Math.random() * depth);
+    let customRects : CustomRect3D[] = [init];
+
+    for(let i =0; i < 4; i++) {
+      const randomCoord = Math.floor(Math.random() * width);
       const randomAxis = sample(["X", "Y", "Z"]);
-      const cut = cutInXY([customRect3D], 400);
-      const cut2 = cutInXZ(cut, 100);
-      const cut3 = cutInYZ(cut2, 150);
-      const cut4 = cutInXY(cut3, 250);
-    //}
-    setCustomRects3D(cut4);
+      customRects = cutIn(customRects, randomAxis, randomCoord);
+    }
+    customRects = subCutIn(customRects, "Z", 250);
+    setCustomRects3D(customRects);
   }
 
   return { generate, customRects3D, setWidth, setHeight, width, height };
