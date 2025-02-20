e321262158f66c79ef3b6ded051c94ddea14a567
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-02-20 19:47:58 +0100

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer({  }: FooterProps) {
   return (
     <footer className="flex flex-row items-center p-4">
       <div className="flex items-center">
-        <span>Made by Guillaume Gomez - 2024</span>
+        <span>Made by Guillaume Gomez - (2024 - {new Date().getFullYear()})</span>
       </div> 
     </footer>
   )
